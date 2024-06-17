9412d5fe313bb9dc2de5b7bc0eada085969634ce
Bipana <bipanashrestha435@gmail.com>
2024-06-17 16:07:37 +0545

    mens,women,children

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
+import Footer from './Components/Footer/Footer';
+import men from './Components/Assets/men.jpg'
+import women from './Components/Assets/women.jpg';
+import kids from './Components/Assets/kids.jpg';
 
+ 
 function App() {
   return (
     <div>
@@ -14,15 +19,16 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop />} />
-          <Route path='/mens' element={<ShopCategory category="men" />} />
-          <Route path='/womens' element={<ShopCategory category="women" />} />
-          <Route path='/kids' element={<ShopCategory category="kid" />} />
+          <Route path='/mens' element={<ShopCategory banner={men} category="men" />} />
+          <Route path='/womens' element={<ShopCategory banner={women} category="women" />} />
+          <Route path='/kids' element={<ShopCategory banner={kids} category="kid" />} />
           <Route path='/product' element={<Product />}>
             <Route path=':productId' element={<Product />} />
           </Route>
           <Route path='/cart' element={<Cart />} />
           <Route path='/login' element={<LoginSignup />} />
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
---
+++ src/Components/Assets/all_product.js
@@ -0,0 +1,88 @@
+import img1 from './img1.jpg'
+import img2 from './img2.jpg'
+import img3 from './img3.jpg'
+import img4 from './img4.jpg'
+import img5 from './img5.jpg'
+import img6 from './img6.jpg'
+import img7 from './img7.jpg'
+import img8 from './img8.png'
+import img9 from './img9.png'
+
+let data_product = [
+    {
+        id:1,
+        name: "Boys Orange Colorblocked Hooded Sweatshirt",
+        category: "kid",
+        Image: img1,
+        new_price: 50.00,
+        old_price:80.50,
+
+    },
+    {
+        id:2,
+        name: "Boys Orange Colorblocked Hooded Sweatshirt",
+        category: "men",
+        Image: img2,
+        new_price: 50.00,
+        old_price:70.50,
+
+    },
+    {
+        id:3,
+        name: "Boys Orange Colorblocked Hooded Sweatshirt",
+        category: "women",
+        Image: img3,
+        new_price: 50.00,
+        old_price:80.50,
+
+    },
+    {
+        id:4,
+        name: "Boys Orange Colorblocked Hooded Sweatshirt",
+        category: "kid",
+        Image: img4,
+        new_price: 50.00,
+        old_price:90.50,
+
+    },
+    {
+        id:5,
+        name: "Boys Orange Colorblocked Hooded Sweatshirt",
+        category: "kid",
+        Image: img3,
+        new_price: 50.00,
+        old_price:80.50,
+
+    },
+    {
+        id:6,
+        name: "Boys Orange Colorblocked Hooded Sweatshirt",
+        category: "kid",
+        Image: img6,
+        new_price: 50.00,
+        old_price:80.50,
+
+    },
+    {
+        id:7,
+        name: "Boys Orange Colorblocked Hooded Sweatshirt",
+        category: "kid",
+
+        Image: img7,
+        new_price: 50.00,
+        old_price:80.50,
+
+    },
+    {
+        id:8,
+        name: "Boys Orange Colorblocked Hooded Sweatshirt",
+        category: "kid",
+        Image: img8,
+        new_price: 50.00,
+        old_price:80.50,
+
+    },
+
+];
+
+export default data_product;  
